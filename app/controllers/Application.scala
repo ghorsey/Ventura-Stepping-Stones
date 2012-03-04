a7473dfb47f4df14d8dcd840957a44a617a991c8
Geoff <geoff@shhhinnovations.com>
2012-03-04 12:20:14 -0800

    translating index page from Asp.Net MVC 3 to Scala Play 2.0

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,16 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def photos = Action { 
+    Ok("Photos")
+  }
+
+  def rules = Action {
+    Ok("Rules")
+  }
+
+  def location = Action {
+    Ok("location")
+  }
 }
